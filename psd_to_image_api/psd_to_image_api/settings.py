7ac33fae43237c3ba3ff973e058f44bb0b8c161d
VJ Pranay <pranaymadasi1@gmail.com>
2020-07-30 10:18:35 +0530

    nw

--- psd_to_image_api/psd_to_image_api/settings.py
+++ psd_to_image_api/psd_to_image_api/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'webapi',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,3 +119,4 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+CORS_ORIGIN_ALLOW_ALL = True
